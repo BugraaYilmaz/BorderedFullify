2e9332e8e6a19e725e4f60e110654a78b922f71e
Buğra Yılmaz <bryl.bugra@gmail.com>
2024-03-05 13:27:11 +0300

    Auto timer

--- app.js
+++ app.js
@@ -1,63 +1,79 @@
 class VideoUrl {
   #currentPageUrl;
-  get currentPageUrl(){
+  get currentPageUrl() {
     return this.#currentPageUrl;
   }
-  constructor(){
+  constructor() {
     this.setPageUrl();
   }
-  setPageUrl(){
-    this.#currentPageUrl=window.location.href;
+  setPageUrl() {
+    this.#currentPageUrl = window.location.href;
   }
-  insertInUrl(){
-    if(this.#currentPageUrl.includes("youtube.com/watch?v")){
-      const partArray = this.#currentPageUrl.split("?"); 
-      const newUrl=partArray[0]+"_popup?"+partArray[1];
-      window.location.href=newUrl;
-      this.#currentPageUrl=newUrl;
+  calculateCurrentSec(){
+    let inSeconds=0;
+    const currentTimeVal=document.querySelector(".ytp-time-current").innerHTML;
+    const currentTimeArray= currentTimeVal.split(":");
+    switch (currentTimeArray.length) {
+      case 4:
+        inSeconds=parseInt(currentTimeArray[0]*86,400)+parseInt(currentTimeArray[1]*3600)+parseInt(currentTimeArray[2]*60)+parseInt(currentTimeArray[3]);
+        break;
+      case 3:
+        inSeconds=parseInt(currentTimeArray[0]*3600)+parseInt(currentTimeArray[1]*60)+parseInt(currentTimeArray[2]);
+        break;
+      case 2:
+        inSeconds=parseInt(currentTimeArray[0]*60)+parseInt(currentTimeArray[1]);
     }
-    else if(this.#currentPageUrl.includes("www.youtube.com/embed/")){
-      const partArray = this.#currentPageUrl.split("/embed/");
-      const newUrl= partArray[0] + "/watch?v=" + partArray[1];
-      window.location.href=newUrl;
-      this.#currentPageUrl=newUrl;
-    }
-    else{
-      console.log("Url Error");
+    return inSeconds;
+  }
+  insertInUrl() {
+    this.#currentPageUrl=this.currentPageUrl.split("&")[0];
+    if (this.#currentPageUrl.includes('youtube.com/watch?v')) {
+      const partArray = this.#currentPageUrl.split('?');
+      const newUrl = partArray[0] + '_popup?' + partArray[1] +"&start="+this.calculateCurrentSec()+"&autoplay=1";
+      window.location.href = newUrl;
+      this.#currentPageUrl = newUrl;
+    } else if (this.#currentPageUrl.includes('www.youtube.com/embed/')) {
+      const partArray = this.#currentPageUrl.split('/embed/');
+      const newUrl = partArray[0] + '/watch?v=' + partArray[1]+"&t="+this.calculateCurrentSec();
+      window.location.href = newUrl;
+      this.#currentPageUrl = newUrl;
+    } else {
+      console.log('Url Error');
     }
   }
-
 }
 
-class InputListener{
+class InputListener {
   urlHandler;
-  constructor(){
-    this.urlHandler = new VideoUrl;
+  constructor() {
+    this.urlHandler = new VideoUrl();
   }
-  listenKeys(){
-    window.addEventListener("keydown",(eventName)=>{
-      switch(eventName.key){
-        case "b":
-        case "B":
+  listenKeys() {
+    window.addEventListener('keydown', (eventName) => {
+      switch (eventName.key) {
+        case 'b':
+        case 'B':
           this.urlHandler.setPageUrl();
           this.urlHandler.insertInUrl();
           break;
       }
-    })
+    });
   }
 }
 
-const listenInputs= new InputListener;
+// const rightPlayerButtons = document.querySelector('.ytp-right-controls');
+// const maximizeButton = document.createElement('img');
+// maximizeButton.setAttribute(
+//   'src',
+//   chrome.runtime.getURL('assets/images/maximize.png')
+// );
+// maximizeButton.setAttribute('style', 'width:48px; height:48px;');
+// rightPlayerButtons.append(maximizeButton);
+// maximizeButton.addEventListener('click', () => {
+//   listenInputs.urlHandler.setPageUrl();
+//   listenInputs.urlHandler.insertInUrl();
+// });
+
+
+const listenInputs = new InputListener();
 listenInputs.listenKeys();
-
-
-
-
-
-const rightPlayerButtons = document.querySelector(".ytp-right-controls");
-const maximizeButton= document.createElement("img");
-maximizeButton.setAttribute("src","assets/images/maximize.png");
-maximizeButton.setAttribute("style","width:48px; height:48px;");
-rightPlayerButtons.append(maximizeButton);
-console.log(rightPlayerButtons);  
-
